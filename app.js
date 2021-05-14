2bc1a824025463cedbd6ab6cecd32e74194367d8
Loharano <mianalaloharano@gmail.com>
2021-05-14 20:13:41 +0300

    setup index router

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var cookieParser = require("cookie-parser")
 var logger = require("morgan")
 
 var fileRouter = require("./routes/file")
+var indexRouter = require("./routes/index")
 
 var app = express()
 
@@ -20,6 +21,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/file", fileRouter)
+app.use("/", index)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
