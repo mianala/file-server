40af6c5f3898ab172757ee02e481d1809b2f7847
Loharano <mianalaloharano@gmail.com>
2021-05-14 19:24:43 +0300

    renamed upload to mef_courriel_upload for futur upload options

--- routes/file.js
+++ routes/file.js
@@ -2,9 +2,13 @@ var express = require("express")
 var multer = require("multer")
 var router = express.Router()
 
-var upload = multer({ dest: "uploads/" })
+var mef_courriel_upload = multer({ dest: "uploads/mef-courriel" })
 
 /* GET home page. */
-router.post("/upload", upload.array("files", 10), function (req, res, next) {})
+router.post(
+  "/upload",
+  mef_courriel_upload.array("files", 10),
+  function (req, res, next) {}
+)
 
 module.exports = router
