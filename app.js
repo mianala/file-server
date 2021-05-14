c52782e1a0741b3bb8e27e5529eaa0ea42d9ba07
Loharano <mianalaloharano@gmail.com>
2021-05-14 20:23:48 +0300

    fix index->indexrouter

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(cookieParser())
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/file", fileRouter)
-app.use("/", index)
+app.use("/", indexRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
